3bb9655ddbd97506c97dcacda6e8e3e87ed54531
thoarrow <38552644+ThoArrow@users.noreply.github.com>
2021-05-02 09:18:08 +0700

    beta

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,42 @@
+const menuToggle = document.querySelector(".header-menu-toggle")
+const menuBar = document.querySelector(".header-menu")
+const serviceItem = document.querySelector(".wedo-item")
+menuToggle.addEventListener("click", function () {
+    menuBar.classList.add("is-expand");
+})
+window.addEventListener("click", function (e) {
+    if (!menuBar.contains(e.target) && !e.target.matches(".header-menu-toggle"))
+        menuBar.classList.remove("is-expand");
+})
+
+window.addEventListener("click", function (e) {
+    if (e.target.classList.contains("wedo-header") || e.target.parentElement.classList.contains("wedo-header")) {
+        document.querySelectorAll(".wedo-body").forEach(el => {
+            if (el.parentElement.contains(e.target)) return
+            let icon = el.parentElement.querySelector(".wedo-header-icon")
+            icon.classList.remove("fa-chevron-down")
+            icon.classList.add("fa-chevron-up")
+            el.classList.remove("is-active")
+        })
+        let bodyContent = e.target.parentElement.querySelector(".wedo-body")
+        if (!bodyContent)
+            bodyContent = e.target.parentElement.parentElement.querySelector(".wedo-body")
+        if (!bodyContent.classList.contains("is-active")) {
+            let icon = e.target.parentElement.querySelector(".wedo-header-icon")
+            icon.classList.remove("fa-chevron-up")
+            icon.classList.add("fa-chevron-down")
+            bodyContent.classList.add("is-active")
+        }
+        else {
+            let icon = e.target.parentElement.querySelector(".wedo-header-icon")
+            bodyContent.classList.remove("is-active");
+            icon.classList.add("fa-chevron-up")
+            icon.classList.remove("fa-chevron-down")
+        }
+    }
+})
+// sYfnzDWGS8!
+
 // Slick slider
 // $(document).ready(function () {
 //   $(".quote-list").slick({
